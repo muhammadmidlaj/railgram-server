5b3e4c084e98f56bb125f79c5b0982bde8dac707
muhammadmidlaj <123820788+muhammadmidlaj@users.noreply.github.com>
2023-05-11 14:14:53 +0530

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,12 +1,19 @@
 const mysql = require('mysql');
 
 //creates a mysql connection
+// const dbConn = mysql.createConnection({
+//     host : 'localhost',
+//     port : '3306',
+//     user : 'root',
+//     password : 'root',
+//     database : 'railgram',
+// });
 const dbConn = mysql.createConnection({
-    host : 'localhost',
+    host : 'railgram.ckognyj4pqrn.us-east-1.rds.amazonaws.com',
     port : '3306',
-    user : 'root',
-    password : 'root',
-    database : 'railgram',
+    user : 'myusername',
+    password : 'mypassword',
+    database : 'railgramaws',
 });
 
 dbConn.connect(function(err){
